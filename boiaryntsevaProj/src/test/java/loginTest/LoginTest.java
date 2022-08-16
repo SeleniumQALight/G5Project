5ae97ce3ac86ac9cb5a9d4ef994c24ad92de0af1
boiaryntseva <boiaryntseva@gmail.com>
2022-08-16 14:52:12 -0400

    Login Test is added, pom is updated

---
+++ boiaryntsevaProj/src/test/java/loginTest/LoginTest.java
@@ -0,0 +1,56 @@
+package loginTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+    WebDriver webDriver;
+
+    @Test
+    public void validLogin() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
+        System.out.println("Site was opened");
+        WebElement inputLogin = webDriver.findElement(By
+                .xpath(".//input[@name='username' and @placeholder='Username']"));
+        inputLogin.clear();
+        inputLogin.sendKeys("qaauto");
+        System.out.println("Qaauto was enterd");
+
+        WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+        inputPassword.clear();
+        inputPassword.sendKeys("123456qwerty");
+        System.out.println("Pass is sent");
+
+        webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
+        System.out.println("Sign in button is clicked");
+// .//button[text()='Sign Out']
+
+        //  WebElement buttonSignOut=webDriver.findElement(By.xpath(".//button[text()='Sign Out']"));
+        Assert.assertTrue("Sign out Button is not visible", isButtonSignedOutDisplayed());
+
+        webDriver.quit();
+        System.out.println("Driver was closed");
+    }
+
+    private boolean isButtonSignedOutDisplayed() {
+        try {
+            WebElement buttonSignOut = webDriver.findElement(By.xpath(".//button[text()='Sign Out']"));
+            return buttonSignOut.isDisplayed();
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+}
