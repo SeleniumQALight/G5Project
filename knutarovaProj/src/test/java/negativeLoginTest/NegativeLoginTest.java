62100f96607148eddca736a59b195aeaa144bba2
Iryna Knutarova <irynajobmail@gmail.com>
2022-08-18 16:33:50 +0300

    HW - negative login test

---
+++ knutarovaProj/src/test/java/negativeLoginTest/NegativeLoginTest.java
@@ -0,0 +1,52 @@
+package negativeLoginTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class NegativeLoginTest {
+    WebDriver webDriver;
+
+    @Test
+    public void validLogin() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
+        System.out.println("Site was opened");
+
+        WebElement inputLogin = webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"));
+        inputLogin.clear();
+        inputLogin.sendKeys("qaauto");
+        System.out.println("qaauto was inputted into login input");
+
+        WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+        inputPassword.clear();
+        inputPassword.sendKeys("123456qwerty123456");
+        System.out.println("password was inputted");
+
+        webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
+        System.out.println("Sign In was clicked");
+
+        Assert.assertTrue("Message 'Invalid username / password' is not visible", isAMessageInvalidUsernamePassword());
+
+        webDriver.quit();
+        System.out.println("Browser was closet");
+    }
+
+    private boolean isAMessageInvalidUsernamePassword(){
+        try {
+            WebElement message = webDriver.findElement(By.xpath(".//div[contains(text(),'Invalid username / password')]"));
+            return message.isDisplayed();
+        }catch (Exception e) {
+            return false;
+        }
+    }
+}
