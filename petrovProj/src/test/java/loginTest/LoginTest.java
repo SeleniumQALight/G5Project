46b6f5d82d7c85d64e95a4d7e5008628f597754d
Andrey Petrov <psandriy@ukr.net>
2022-08-18 17:39:53 +0300

    add inValid logan test

--- petrovProj/src/test/java/loginTest/LoginTest.java
+++ petrovProj/src/test/java/loginTest/LoginTest.java
@@ -25,7 +25,7 @@ public class LoginTest {
 
         WebElement inputLogin = webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"));
         inputLogin.clear();
-        inputLogin.sendKeys("qaauto1");
+        inputLogin.sendKeys("qaauto");
         System.out.println("qaauto was inputted into login input");
 
         WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
@@ -43,7 +43,6 @@ public class LoginTest {
         System.out.println("Browser was closed");
     }
 
-
     private boolean isButtonSignOutDisplayed(){
         try{
             WebElement buttonSingOut = webDriver.findElement(By.xpath(".//button[text()='Sign Out']"));
@@ -53,4 +52,46 @@ public class LoginTest {
         }
 
     }
+
+    @Test
+    public void inValidLogin(){
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("https://qa-complex-app-for-testing.herokuapp.com/");
+        System.out.println("Site opened");
+
+        WebElement inputLogin = webDriver.findElement(By.xpath(".//input[@name='username' and @placeholder='Username']"));
+        inputLogin.clear();
+        inputLogin.sendKeys("qaauto");
+        System.out.println("inValid login - qaauto1 was inputted into login input");
+
+        WebElement inputPassword = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
+        inputPassword.clear();
+        inputPassword.sendKeys("123456qwerty");
+        System.out.println("password 123456qwerty was inputted");
+
+        webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
+        System.out.println("Sing In was clicked");
+
+        Assert.assertTrue("Button Sign Out is visible", isMessageErrorLoginOrPassword());
+
+        System.out.println("InValid test passed");
+        webDriver.quit();
+        System.out.println("Browser was closed");
+    }
+
+    private boolean isMessageErrorLoginOrPassword(){
+        try{
+            WebElement messageErrorLoginOrPassword = webDriver.findElement(By.xpath(".//div[text()='Invalid username / pasword']"));
+            return messageErrorLoginOrPassword.isDisplayed();
+        }catch (Exception e){
+            return false;
+        }
+
+    }
+
+
 }
