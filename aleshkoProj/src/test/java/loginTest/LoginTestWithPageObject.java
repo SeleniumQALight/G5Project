dc0959445a7a0a26294a4131299fc79d1d2ca8ad
Aleshko_Kyryl <aleshko.k@gmail.com>
2022-08-19 14:45:41 +0300

    invalidLogin tests were added in simple and pageObject forms

--- aleshkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ aleshkoProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -3,19 +3,34 @@ package loginTest;
 import baseTest.BaseTest;
 import org.junit.Assert;
 import org.junit.Test;
-
+import pages.HomePage;
+import pages.LoginPage;
 
 public class LoginTestWithPageObject extends BaseTest {
 
     @Test
     public void validLogin() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        HomePage homePage = new HomePage(webDriver);
+
         loginPage.openLoginPage();
         loginPage.enterUsernameIntoLoginInput("qaauto");
         loginPage.enterPasswordIntoPasswordInput("123456qwerty");
         loginPage.clickOnSignInButton();
 
         Assert.assertTrue("Button Sign Out is not visible", homePage.isButtonSignOutDisplayed());
-
     }
 
+    @Test
+    public void invalidLogin() {
+        LoginPage loginPage = new LoginPage(webDriver);
+
+        loginPage.openLoginPage();
+        loginPage.enterUsernameIntoLoginInput("qaauto");
+        loginPage.enterPasswordIntoPasswordInput("654321qwerty");
+        loginPage.clickOnSignInButton();
+
+        Assert.assertTrue("Alert about wrong username/password is not visible", loginPage.isAlertDisplayed());
+        Assert.assertTrue("Button \"Sign In\" is not visible", loginPage.isButtonSignInDisplayed());
+    }
 }
