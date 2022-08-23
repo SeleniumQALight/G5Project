2e7ee2d8fb53417d547e18e83baf0fd994d65a41
shushkovaira <shushkovaira@gmail.com>
2022-08-23 18:22:44 +0300

    LoginTestWithPageObject

--- shushkovaProj/src/test/java/loginTest/InvalidLogin.java
+++ shushkovaProj/src/test/java/loginTest/InvalidLogin.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class invalidLogin {
+public class InvalidLogin {
     WebDriver webDriver;
     @Test
     public void invalidLogin(){
