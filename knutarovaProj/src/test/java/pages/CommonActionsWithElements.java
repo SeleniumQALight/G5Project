675fc1578f4661a798c998e85681a52b4bb0a68e
Iryna Knutarova <irynajobmail@gmail.com>
2022-08-23 21:27:21 +0300

    CommonActionsWithElements was created

---
+++ knutarovaProj/src/test/java/pages/CommonActionsWithElements.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class CommonActionsWithElements {
+    WebDriver webDriver;
+    Logger logger = Logger.getLogger(getClass());
+
+    public CommonActionsWithElements(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+    }
+
+    protected void enterTextIntoElement(WebElement webElement, String text){
+        try{
+            webElement.clear();
+            webElement.sendKeys(text);
+            logger.info("'" + text + "' was inputted into '" + webElement.getAccessibleName() + "'");
+        }catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
+    protected void clickOnElement(WebElement webElement){
+        try{
+            String name = webElement.getAccessibleName();
+            webElement.click();
+            logger.info("'" + name + "' was clicked");
+        }catch (Exception e){
+            printErrorAndStopTest(e);
+        }
+    }
+
+    private void printErrorAndStopTest(Exception e) {
+        logger.error("Can't work with element " + e);
+        Assert.fail("Can't work with element " + e);
+    }
+}
