5dc5cd331bb0eb7643e07d7feac36fe6824977f3
ross <rosko48@gmail.com>
2022-08-23 21:33:21 +0300

    Common action with elements was created

---
+++ kolesnykProj/src/test/java/postTest/CreatePostTest.java
@@ -0,0 +1,12 @@
+package postTest;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class CreatePostTest extends BaseTest {
+
+    @Test
+    public void createNewPost(){
+        
+    }
+}
