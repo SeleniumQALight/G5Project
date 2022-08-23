1e5ac0a2d9c89898bce4f54aaa135549a615ae5f
boiaryntseva <boiaryntseva@gmail.com>
2022-08-23 14:35:16 -0400

    CommonActionsWithElements is added CreatePostTest is added

--- boiaryntsevaProj/src/main/java/pages/ParentPage.java
+++ boiaryntsevaProj/src/main/java/pages/ParentPage.java
@@ -2,12 +2,11 @@ package pages;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
-public class ParentPage {
-    WebDriver webDriver;
-    Logger logger = Logger.getLogger(getClass());
+public class ParentPage extends CommonActionsWithElements {
 
     public ParentPage(WebDriver webDriver) {
-        this.webDriver = webDriver;
+        super(webDriver);
     }
 }
