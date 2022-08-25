f0c12808993ac1f30a4556a695d8e227d8dbfdd4
Ndividual17 <ndividualkim17@gmail.com>
2022-08-25 21:35:08 +0300

    CreatePostTest part1

--- yakymenkoProj/src/main/java/pages/CommonActionsWithElements.java
+++ yakymenkoProj/src/main/java/pages/CommonActionsWithElements.java
@@ -5,9 +5,10 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class CommonActionsWithElements {
-    WebDriver webDriver;
+    protected WebDriver webDriver;
     Logger logger = Logger.getLogger(getClass()); // Alt+Insert
 
     public CommonActionsWithElements(WebDriver webDriver) {
@@ -38,6 +39,59 @@ public class CommonActionsWithElements {
         }
     }
 
+    /**
+     * Метод поверне true якщо елемент показаний
+     * або поверне false якщо елемент не показаний або його взагалі немає
+     *
+     * @param webElement
+     * @return
+     */
+    protected boolean isElementDisplayed(WebElement webElement) {
+        try {
+            boolean state = webElement.isDisplayed();
+            String message;
+            if (state) {
+                message = "Element is displayed";
+            } else {
+                message = "Element is Not displayed";
+            }
+            logger.info(message);
+            return state;
+        } catch (Exception e) {
+            logger.info("Element is Not displayed");
+            return false;
+        }
+    }
+
+    /**
+     * Вибираємо значення в ДропДауні по видимому тексту
+     * @param dropDown
+     * @param text - видимий текст
+     */
+    protected void selectTextInDropDown(WebElement dropDown, String text) {
+        try {
+            Select select = new Select(dropDown);
+            select.selectByVisibleText(text);
+            logger.info("'" + text + "' was selected in DropDown");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    /**
+     * Вибираємо значення в ДропДауні по value
+     * @param dropDown
+     * @param value - value
+     */
+    protected void selectValueInDropDown(WebElement dropDown, String value) {
+        try {
+            Select select = new Select(dropDown);
+            select.selectByValue(value);
+            logger.info("'" + value + "' was selected in DropDown");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
 
     private void printErrorAndStopTest(Exception e) {
         logger.error("Can not work with element " + e);
--- yakymenkoProj/src/test/java/postTest/CreatePostTest.java
+++ yakymenkoProj/src/test/java/postTest/CreatePostTest.java
@@ -5,7 +5,16 @@ import org.junit.Test;
 
 public class CreatePostTest extends BaseTest {
     @Test
-    public void createNewPost() {
+    public void createNewPost() { //alt+Enter → HomePage
+        homePage // метод написання ланцюгів
+                .openHomePage()
+                .getHeaderElement().clickOnButtonCreatePost() //alt+enter → HomePage
+                .checkIsRedirectToCreatePostPage()
+                .enterTextInInputTitle("yakymenko-post")
+//                .selectTextInDropDownRole("Приватне повідомлення") //alt+enter → CreatePostPage
+                .selectValueInDropDownRole("One Person")
+        ;
+
 
     }
 }
