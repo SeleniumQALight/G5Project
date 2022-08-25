d0eb6a60e60d270ed11722139e3ba047bad7761d
Andrey Petrov <psandriy@ukr.net>
2022-08-25 21:34:52 +0300

    CreatePostTest part1

---
+++ petrovProj/src/main/java/pages/CreatePostPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CreatePostPage extends ParentPage{
+    @FindBy(name = "title")
+    private WebElement inputTitle;
+
+    @FindBy(tagName="select")
+    private WebElement dropDownRole;
+
+    public CreatePostPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public CreatePostPage checkIsRedirectToCreatePage(){
+        //TODO check url
+        Assert.assertTrue("Page Create post is not loaded", isElementDisplayed(inputTitle));
+        return  this;
+    }
+
+    public CreatePostPage enterTextInInputTitle(String title){
+        enterTextIntoElement(inputTitle, title);
+
+        return this;
+    }
+
+    public CreatePostPage selectTextInDropDownRole(String textForSelect) {
+        selectTextInDropDown(dropDownRole, textForSelect);
+        return this;
+    }
+
+    public CreatePostPage selectValueInDropDownRole(String valueForSelect) {
+        selectValueInDropDown(dropDownRole, valueForSelect);
+        return this;
+    }
+
+}
--- petrovProj/src/main/java/pages/HomePage.java
+++ petrovProj/src/main/java/pages/HomePage.java
@@ -1,10 +1,14 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import pages.elements.HeaderElement;
 
 public class HomePage extends ParentPage{
+    private HeaderElement headerElement = new HeaderElement(webDriver);
+
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -17,5 +21,35 @@ public class HomePage extends ParentPage{
             return false;
         }
 
+    }
+
+    /**
+     * Проверка, ганрузилась ли HomePage
+     * @return
+     */
+    public HomePage checkIsRedirectToHomePage(){
+        //TODO checkURL
+        Assert.assertTrue("HomePage did not load",isButtonSignOutDisplayed());
+        return this;
+    }
+
+    /**
+     * метод открывает HomePage и проверяет что именно она открыта.
+     * @return
+     */
+    public HomePage openHomePage() {
+        LoginPage loginPage = new LoginPage(webDriver);
+        loginPage.loginWithValidCred();
+        checkIsRedirectToHomePage();
+
+        return this;
+    }
+
+    /**
+     * отдает приветные елемент хедера
+     * @return
+     */
+    public HeaderElement getHeaderElement() {
+        return headerElement;
     }
 }
