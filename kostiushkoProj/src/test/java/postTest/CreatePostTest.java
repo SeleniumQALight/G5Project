cc19969a6c4d0b54c45f48d8e16b9460f9a7e143
KostiushkoDenis <kostiushko.denis@gmail.com>
2022-08-29 20:16:08 +0300

    Kostiushko-28-08-HW  №2

--- kostiushkoProj/src/test/java/postTest/CreatePostTest.java
+++ kostiushkoProj/src/test/java/postTest/CreatePostTest.java
@@ -23,33 +23,8 @@ public class CreatePostTest extends BaseTest {
             .selectTextInDropDownByUI("Групове повідомлення")
             .enterTextInTextAreaBodyContent("Text for body")
             .clikSaveNewPostButton()
-
-
-
             ;
     }
 
 
 }
-
-
-//    protected void selectTextInDropDownByUI (String text){
-//        try {
-//            webDriver.findElement(By.xpath(".//*[@value='All Users']")).click();
-//            webDriver.findElement(By.xpath(".//*[contains(text(),'"+text+"')]")).click();
-//            logger.info("'" + text + " ' was selected in DropDown");
-//
-//        }catch (Exception e){
-//            prinErrorAndStopTest(e);
-//        }
-//    }
-//
-//    public CreatePostPage enterTextInTextAreaBodyContent(String text){
-//        enterTextIntoElement(postBody, text);
-//        return this;
-//    }
-//
-//    public CreatePostPage clikSaveNewPostButton (String textForSelect) {
-//        this.selectTextInDropDown(dropDownRole, textForSelect);
-//        return this;
-//    }
