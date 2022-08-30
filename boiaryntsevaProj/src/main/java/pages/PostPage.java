e6309bae9986b2597d977ce5e0a35ba03a95a7d3
boiaryntseva <boiaryntseva@gmail.com>
2022-08-30 14:45:05 -0400

    Post Page is updated, waits are added

--- boiaryntsevaProj/src/main/java/pages/PostPage.java
+++ boiaryntsevaProj/src/main/java/pages/PostPage.java
@@ -1,9 +1,39 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pages.elements.HeaderElement;
 
 public class PostPage extends ParentPage {
+
+
+    private HeaderElement headerElement = new HeaderElement(webDriver);
+
     public PostPage(WebDriver webDriver) {
+
         super(webDriver);
+    }
+
+    public HeaderElement getHeaderElement() {
+        return headerElement;
+    }
+
+    @FindBy(xpath = ".//a[@data-original-title='Edit']")
+    private WebElement buttonEdit;
+
+    @FindBy(xpath = ".//*[@class='alert alert-success text-center']")
+    private WebElement alertSuccess;
+
+    public PostPage checkIsRedirectToPostPage() {
+        //TODO check URL
+        Assert.assertTrue("Post page is not loaded", isElementDisplayed(buttonEdit));
+        return this;
+    }
+
+    public PostPage checkTextInAlert(String text) {
+        Assert.assertEquals("Text in alert", text, alertSuccess.getText());
+        return this;
     }
 }
