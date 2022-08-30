896203dfe77a106bb4abd1cc453132025cfcf518
KateDrebot <73437573+KateDrebot@users.noreply.github.com>
2022-08-30 14:42:54 -0700

    CreatePostTest part2

--- drebotProj/src/main/java/pages/ParentPage.java
+++ drebotProj/src/main/java/pages/ParentPage.java
@@ -1,11 +1,15 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ParentPage extends CommonActionsWithElements {
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
     }
 
-
+    protected void waitChatToBeHide() {
+        webDriverWait10.withMessage("cat is not closed").until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(".//*[@id='chat-wrapper']")));
+    }
 }
--- drebotProj/src/main/java/pages/elements/HeaderElement.java
+++ drebotProj/src/main/java/pages/elements/HeaderElement.java
@@ -1,11 +1,11 @@
 package pages.elements;
 
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import pages.CommonActionsWithElements;
 import pages.CreatePostPage;
+import pages.MyProfilePage;
 
 public class HeaderElement extends CommonActionsWithElements {
     @FindBy(xpath = ".//a[@class='btn btn-sm btn-success mr-2']")
@@ -13,6 +13,9 @@ public class HeaderElement extends CommonActionsWithElements {
 
     @FindBy(xpath = ".//button[@class='btn btn-sm btn-secondary']")
     private WebElement buttonSignOut;
+
+    @FindBy(xpath = ".//a[@class='mr-2']")
+    private WebElement buttonMyProfile;
 
 
     public HeaderElement(WebDriver webDriver) {
@@ -26,6 +29,11 @@ public class HeaderElement extends CommonActionsWithElements {
 
     public boolean isButtonSignOutDisplayed() {
         return isElementDisplayed(buttonSignOut);
+    }
+
+    public MyProfilePage clickOnButtonMyProfile() {
+        clickOnElement(buttonMyProfile);
+        return new MyProfilePage(webDriver);
     }
 }
 
