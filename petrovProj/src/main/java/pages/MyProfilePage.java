d0457b64fc8e694647646324b2df5d8171d109ad
Andrey Petrov <psandriy@ukr.net>
2022-09-01 22:05:33 +0300

    create post test part 3

--- petrovProj/src/main/java/pages/MyProfilePage.java
+++ petrovProj/src/main/java/pages/MyProfilePage.java
@@ -1,14 +1,21 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class MyProfilePage extends PostPage{
     @FindBy(xpath = ".//img[@class='avatar-small']")
     private WebElement avatarOnProfilePage;
 
+    private String postTitleLocators = ".//*[text()='%s']";
+
+    @FindBy(xpath = ".//*[text()='Post successfully deleted']")
+    private WebElement successDeletePostMessage;
 
 
     public MyProfilePage(WebDriver webDriver) {
@@ -20,5 +27,39 @@ public class MyProfilePage extends PostPage{
         waitChatToBeHide();
         Assert.assertTrue(" MyProfilePage is not loaded", isElementDisplayed(avatarOnProfilePage));
         return  this;
+    }
+
+    public MyProfilePage checkPostWasCreated(String title) {
+        List<WebElement> postList = getPostListWithTitle(title);
+        Assert.assertEquals("Number of posts with title = "+title, 1, postList.size());
+        return this;
+    }
+
+
+    public MyProfilePage deletePostWithTitleTillPresent(String title) {
+        List<WebElement> listPost = getPostListWithTitle(title);
+        int counter = 0;
+        while (!listPost.isEmpty() && counter<100){
+            clickOnElement(webDriver.findElement(By.xpath(String.format(postTitleLocators, title))));
+            new PostPage(webDriver)
+                    .checkIsRedirectToPostPage()
+                    .clickOnDeleteButton()
+                  .checkIsRedirectToMyProfilePage()
+                    .checkIsSuccessDeletePostMessagePresent();
+            logger.info("Post was deleted with title " + title);
+            listPost = getPostListWithTitle(title);
+            counter++;
+        }
+        logger.info("all posts were deleted with title " + title);
+        return this;
+    }
+
+    public MyProfilePage checkIsSuccessDeletePostMessagePresent() {
+        Assert.assertTrue("Element is not Displayed ", isElementDisplayed(successDeletePostMessage));
+        return this;
+    }
+
+    private List<WebElement> getPostListWithTitle (String title){
+        return webDriver.findElements(By.xpath(String.format(postTitleLocators, title)));
     }
 }
--- petrovProj/src/test/java/postTests/CreatePostTest.java
+++ petrovProj/src/test/java/postTests/CreatePostTest.java
@@ -1,16 +1,20 @@
 package postTests;
 
 import baseTest.BaseTest;
+import libs.Util;
+import org.junit.After;
 import org.junit.Test;
 
 public class CreatePostTest extends BaseTest {
+    final String TITLE = "petrov-TC1_"+ Util.getDateAndTimeFormatted();
+
     @Test
-    public void createNewPost(){
+    public void TC1_createNewPost(){
         homePage
                 .openHomePage()
                 .getHeaderElement().clickOnButtonCreatePost()
              .checkIsRedirectToCreatePage()
-                .enterTextInInputTitle("petrov-post_v1")
+                .enterTextInInputTitle(TITLE)
                 .enterTextInInputBodyContent("Text into tag textArea on page create post")
                 .selectOptionInDropDownByUI()
  //               .selectValueInDropDownRole("One Person")
@@ -19,10 +23,19 @@ public class CreatePostTest extends BaseTest {
                 .checkTestIsAlert("New post successfully created.")
                 .getHeaderElement().clickOnMyProfileButton()
                 .checkIsRedirectToMyProfilePage()
+                .checkPostWasCreated(TITLE)
 
         ;
 
 //                .selectTextInDropDownRole("Приватне повідомлення")
         ;
+    }
+    @After
+    public void DeletePosts(){
+        homePage
+                .openHomePage()
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .deletePostWithTitleTillPresent(TITLE);
     }
 }
