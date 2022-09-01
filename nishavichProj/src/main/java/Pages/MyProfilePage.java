53be82f159557e386ced27dbaab6f51d3f6ce3ab
fnisa <tpirus@icloud.com>
2022-09-01 20:46:05 +0200

    Nisavic_commit

--- nishavichProj/src/main/java/Pages/MyProfilePage.java
+++ nishavichProj/src/main/java/Pages/MyProfilePage.java
@@ -1,13 +1,22 @@
 package Pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class MyProfilePage extends ParentPage {
     @FindBy(xpath = ".//img[@class=\"avatar-small\"]")
     private WebElement avatar;
+
+    private String postTitleLocator = ".//*[text()='%s']";
+
+    @FindBy(xpath = ".//*[text()='Post successfully deleted']")
+    private WebElement successDeletedPostMessage;
+
 
     public MyProfilePage(WebDriver webDriver) {
         super(webDriver);
@@ -16,5 +25,41 @@ public class MyProfilePage extends ParentPage {
         Assert.assertTrue("my profile is not loaded",
                 isElementDisplayed(avatar));
         return this;
+    }
+
+    public MyProfilePage checkPostWasCreated(String title) {
+        List <WebElement> postList = webDriver.findElements(By.xpath(String.format(postTitleLocator,title)));
+        Assert.assertEquals("Number of Posts  with title" + title,1, postList.size());
+
+        return this;
+    }
+
+    public MyProfilePage deletePostsWithTitleTillPresent(String title) {
+        List <WebElement> listPost = getPostsListWithTitle (title);
+        int counter = 0;
+        while (!listPost.isEmpty()){
+            clickOnElement(webDriver.findElement (By.xpath(String.format(postTitleLocator,title))));
+            new PostPage (webDriver)
+                    .checkIsRedirectToPostPage()
+                    .clickOnDeleteButton()
+                    .checkIsSuccessDeletedPostMessagePresent();
+            logger.info("Post was deleted with title" + title);
+            listPost = getPostsListWithTitle(title);
+            counter++;  //counter = counter +1
+
+        }
+        logger.info("All posts were deleted with title" + title);
+
+        return this;
+    }
+
+    private MyProfilePage checkIsSuccessDeletedPostMessagePresent(){
+        Assert.assertTrue("element is not present", isElementDisplayed (successDeletedPostMessage));
+        return this;
+    }
+    private List <WebElement> getPostsListWithTitle(String title) {
+        return webDriver.findElements(
+                By.xpath(String.format(postTitleLocator,title)));
+
     }
 }
--- nishavichProj/src/test/java/PostTest/CreatePostsTest.java
+++ nishavichProj/src/test/java/PostTest/CreatePostsTest.java
@@ -1,15 +1,18 @@
 package PostTest;
 
+import Libs.Util;
 import baseTest.BaseTest;
+import org.junit.After;
 import org.junit.Test;
 
 public class CreatePostsTest extends BaseTest {
+    final String TITLE = "nisavic_post1" + Util.getDateAndTimeFormatted();
     @Test
     public void createNewPost(){
 homePage.openHomePage()
         .getHeaderElement().clickOnButtonCreatePost()
         . checkIsRedirectToCreatePostPage()
-        .enterTextInputTitle("nisavic-post")
+        .enterTextInputTitle(TITLE)
         .enterTextInputBody("test")
        // .selectTextInFropdown("Приватне повідомлення")
         .selectValueInDropdownRole("One Person")
@@ -18,8 +21,19 @@ homePage.openHomePage()
         .checkTextInAlert("New post successfully created.")
         .getHeaderElement().clickOnMyProfileButton()
         .checkIsRedirectTpMyProfilePage()
+        .checkPostWasCreated(TITLE)
 
 
         ;
     }
-}
+
+    @After
+    public void deletePosts () {
+        homePage
+                .openHomePage()
+                .getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectTpMyProfilePage()
+                .deletePostsWithTitleTillPresent(TITLE);
+    }
+    }
+
