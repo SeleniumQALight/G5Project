32aa5870754b981cb9e88aa08fdc6c6e3596c9a7
Ndividual17 <ndividualkim17@gmail.com>
2022-09-05 00:30:14 +0300

    created checkValidationAlertsInRegistrationForm by HW4 part1

--- yakymenkoProj/src/main/java/pages/LoginPage.java
+++ yakymenkoProj/src/main/java/pages/LoginPage.java
@@ -1,11 +1,14 @@
 package pages;
 
 import libs.TestData;
+import libs.Util;
 import org.junit.Assert;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class LoginPage extends ParentPage { // Alt+Insert↓ - create constructor
     // Описані елементи, які ми робимо через анотацію @FindBy та ініціалізуємо через PageFactory.initElements який ми винесли в цей батьківський клас:
@@ -17,6 +20,14 @@ public class LoginPage extends ParentPage { // Alt+Insert↓ - create constructo
     private WebElement buttonSignIn;
     @FindBy(xpath = ".//div[@class='alert alert-danger text-center']")
     private WebElement alertInvalidUsernamePassword;
+    @FindBy(id = "username-register")
+    private WebElement inputUsernameRegister;
+    @FindBy(id = "email-register")
+    private WebElement inputEmailRegister;
+    @FindBy(id = "password-register")
+    private WebElement inputPasswordRegister;
+    @FindBy(xpath = ".//div[contains(@class, 'alert') and contains(@class, 'visible')]")
+    private List<WebElement> visibleAlert;
 
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
@@ -38,47 +49,22 @@ public class LoginPage extends ParentPage { // Alt+Insert↓ - create constructo
     }
 
     public void enterUserNameIntoLoginInput(String userName) {
-//        try {
-////            WebElement webElement = webDriver.findElement(By.xpath(".//input[@name='username' and " +
-////                    "@placeholder='Username']"));
-//            inputUserNameHeader.clear();
-//            inputUserNameHeader.sendKeys(userName);
-//            logger.info(userName + " was entered into input");
-//
-//        } catch (Exception e) {
-//            printErrorAndStopTest(e);
-//        }
         enterTextIntoElement(inputUserNameHeader, userName); // ↑ замінили на виклик метода
     }
 
     public void enterPasswordIntoInputPassword(String password) {
-//        try {
-////            WebElement webElement = webDriver.findElement(By.xpath(".//input[@placeholder='Password']"));
-//            inputPasswordHeader.clear();
-//            inputPasswordHeader.sendKeys(password);
-//            logger.info(password + " was entered into input");
-//        } catch (Exception e) {
-//            printErrorAndStopTest(e);
-//        }
         enterTextIntoElement(inputPasswordHeader, password);
     }
 
     public void clickOnButtonLogIn() {
-//        try {
-////            webDriver.findElement(By.xpath(".//button[text()='Sign In']")).click();
-//            buttonSignIn.click();
-//            logger.info("Button Sign In was clicked");
-//        } catch (Exception e) {
-//            printErrorAndStopTest(e);
-//        }
         clickOnElement(buttonSignIn);
     }
 
-    public boolean isAlertDisplayed(){
+    public boolean isAlertDisplayed() {
         return isElementDisplayed(alertInvalidUsernamePassword);
     }
 
-    public boolean isButtonSignInDisplayed(){
+    public boolean isButtonSignInDisplayed() {
         return isElementDisplayed(buttonSignIn);
     }
 
@@ -90,8 +76,31 @@ public class LoginPage extends ParentPage { // Alt+Insert↓ - create constructo
         return new HomePage(webDriver);
     }
 
-//    private void printErrorAndStopTest(Exception e) {
-//        logger.error("Can not work with element " + e);
-//        Assert.fail("Can not work with element " + e);
-//    }
+    public void enterUsernameIntoUsernameRegisterInput(String userName) {
+        enterTextIntoElement(inputUsernameRegister, userName);
+    }
+
+    public void enterEmailIntoEmailRegisterInput(String email) {
+        enterTextIntoElement(inputEmailRegister, email);
+    }
+
+    public void enterPasswordIntoPasswordRegisterInput(String password) {
+        enterTextIntoElement(inputPasswordRegister, password);
+    }
+
+    public void checkValidationAlertNumber() {
+        Util.waitABit(3);
+        Assert.assertEquals("Incorrect number of alerts in Register form", 3, visibleAlert.size());
+        logger.info("Number of alerts in Register form: " + visibleAlert.size());
+    }
+
+    public void checkValidationAlertText() {
+        Util.waitABit(3);
+        Assert.assertEquals("Username must be at least 3 characters.", visibleAlert.get(0).getText());
+        logger.info("Visible alert in UsernameRegister: " + visibleAlert.get(0).getText());
+        Assert.assertEquals("You must provide a valid email address.", visibleAlert.get(1).getText());
+        logger.info("Visible alert in EmailRegister: " + visibleAlert.get(1).getText());
+        Assert.assertEquals("Password must be at least 12 characters.", visibleAlert.get(2).getText());
+        logger.info("Visible alert in PasswordRegister: " + visibleAlert.get(2).getText());
+    }
 }
---
+++ yakymenkoProj/src/test/java/loginTest/RegistrationTest.java
@@ -0,0 +1,16 @@
+package loginTest;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class RegistrationTest extends BaseTest {
+    @Test
+    public void checkValidationAlertsInRegistrationForm() {
+        loginPage.openLoginPage();
+        loginPage.enterUsernameIntoUsernameRegisterInput("tr");
+        loginPage.enterEmailIntoEmailRegisterInput("test.com");
+        loginPage.enterPasswordIntoPasswordRegisterInput("123");
+        loginPage.checkValidationAlertNumber();
+        loginPage.checkValidationAlertText();
+    }
+}
