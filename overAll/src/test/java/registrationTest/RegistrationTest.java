5a18c3199f32f1c30e65d35d6a906373e2ee64e6
Taras Radulenko <Taras_Radulenko@epam.com>
2022-09-06 20:41:31 +0300

    RegistrationTest.java part1

---
+++ overAll/src/test/java/registrationTest/RegistrationTest.java
@@ -0,0 +1,31 @@
+package registrationTest;
+
+import org.junit.Test;
+
+import baseTest.BaseTest;
+
+public class RegistrationTest extends BaseTest {
+    final static String ERROR_USERNAME = "Username must be at least 3 characters.";
+    final static String ERROR_EMAIL = "You must provide a valid email address.";
+    final static String ERROR_PASSWORD = "Password must be at least 12 characters.";
+    final static String SEMICOLON = ";";
+    final static String COMMA = ",";
+    final static String SHORT_USER_NAME = "tr";
+
+    String expectedErrors = ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD;
+
+
+    @Test
+    public void registrationErrors(){
+        loginPage
+                .openLoginPage()
+                .enterUserNameIntoRegistrationForm("qaauto1")
+                .enterEmailIntoRegistrationFrom("test.com")
+                .enterPasswordIntoRegistrationForm("123")
+                .checkErrorsMessages(expectedErrors)
+
+        ;
+
+    }
+
+}
