dc83edc779638de02229257c041f1afadc284ece
VYefimchuk <Klunenko.Vika@gmail.com>
2022-09-08 00:22:54 +0300

    RegistrationTest Part1

---
+++ yefimchukProj/src/test/java/registrationTest/RegistrationTest.java
@@ -0,0 +1,30 @@
+package registrationTest;
+
+import baseTest.BaseTest;
+import org.junit.Test;
+
+public class RegistrationTest extends BaseTest {
+    final static String ERROR_USERNAME = "Username must be at least 3 characters.";
+    final static String ERROR_EMAIL = "You must provide a valid email address.";
+    final static String ERROR_PASSWORD = "Password must be at least 12 characters.";
+    final static String SEMICOLON = ";";
+    final static String COMMA = ",";
+    final static String SHORT_USER_NAME = "tr";
+
+    String expectedErrors = ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD;
+
+    @Test
+    public void registrationErrors() {
+        loginPage
+                .openLoginPage()
+                .enterUserNameIntoRegistrationForm(SHORT_USER_NAME)
+                .enterEmailIntoRegistrationForm("test.com")
+                .enterPasswordIntoRegistrationForm("123")
+                .checkErrorsMessages(expectedErrors)
+
+
+        ;
+
+
+    }
+}
