a99d3f55af1c3fd3e7b5a76b13593feb1e49eeac
VYefimchuk <Klunenko.Vika@gmail.com>
2022-09-08 10:53:57 +0300

    RegistrationTest Part3 Params

--- yefimchukProj/src/test/java/registrationTest/RegistrationTest.java
+++ yefimchukProj/src/test/java/registrationTest/RegistrationTest.java
@@ -1,25 +1,39 @@
 package registrationTest;
 
 import baseTest.BaseTest;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
+import libs.TestData;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class RegistrationTest extends BaseTest {
     final static String ERROR_USERNAME = "Username must be at least 3 characters.";
     final static String ERROR_EMAIL = "You must provide a valid email address.";
     final static String ERROR_PASSWORD = "Password must be at least 12 characters.";
+    final static String ERROR_ALREADY_EXIST = "That username is already taken.";
     final static String SEMICOLON = ";";
     final static String COMMA = ",";
     final static String SHORT_USER_NAME = "tr";
 
-    String expectedErrors = ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD;
+    //   String expectedErrors = ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD;
 
     @Test
-    public void registrationErrors() {
+    @Parameters({
+            SHORT_USER_NAME + COMMA + "test.com" + COMMA + "123" + COMMA + ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD
+            , TestData.VALID_LOGIN + COMMA + "qqq" + COMMA + "123" + COMMA + ERROR_ALREADY_EXIST + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD
+            , SHORT_USER_NAME + COMMA + "test@tex.com" + COMMA + "546" + COMMA + ERROR_USERNAME + SEMICOLON + ERROR_PASSWORD
+    })
+    @TestCaseName("registrationErrors: login = {0}, email = {1}, password = {2}")
+    public void registrationErrors(String userName, String email, String password, String expectedErrors) {
         loginPage
                 .openLoginPage()
-                .enterUserNameIntoRegistrationForm(SHORT_USER_NAME)
-                .enterEmailIntoRegistrationForm("test.com")
-                .enterPasswordIntoRegistrationForm("123")
+                .enterUserNameIntoRegistrationForm(userName)
+                .enterEmailIntoRegistrationForm(email)
+                .enterPasswordIntoRegistrationForm(password)
                 .checkErrorsMessages(expectedErrors)
 
 
