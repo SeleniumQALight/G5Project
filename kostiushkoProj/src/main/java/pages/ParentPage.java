45983abf52370e17cfdaf8787b3bc42bbcd110a3
KostiushkoDenis <kostiushko.denis@gmail.com>
2022-09-13 21:26:55 +0300

    Kostiushko-13-09

--- kostiushkoProj/src/main/java/pages/ParentPage.java
+++ kostiushkoProj/src/main/java/pages/ParentPage.java
@@ -1,16 +1,36 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class ParentPage extends CommonActionsWithElements {
+import static org.hamcrest.CoreMatchers.containsString;
+
+abstract class ParentPage extends CommonActionsWithElements {
+    protected String baseUrl;
 
 
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
+        baseUrl = "https://qa-complex-app-for-testing.herokuapp.com";
     }
+
+
+    abstract String getRelateUrl();
+
+    protected void checkUrl(){
+        Assert.assertEquals("Invalid page"
+              ,baseUrl + getRelateUrl()
+              ,webDriver.getCurrentUrl());
+    }
+
+    protected void checkUrlwithPattern(){
+        Assert.assertThat("Invalid page"
+                ,webDriver.getCurrentUrl()
+                ,containsString(baseUrl+getRelateUrl()));
+    }
+
 
     protected void waitChatToBeHide() {
         webDriverWait10.withMessage("Chat is not closed")
