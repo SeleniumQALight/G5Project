18a5551cbf68400592323a983da0653c0e156eeb
KateDrebot <73437573+KateDrebot@users.noreply.github.com>
2022-09-13 12:55:50 -0700

    work with key

--- drebotProj/src/main/java/pages/LoginPage.java
+++ drebotProj/src/main/java/pages/LoginPage.java
@@ -4,9 +4,8 @@ import libs.TestData;
 import libs.Util;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -169,4 +168,6 @@ public class LoginPage extends ParentPage {
 
         return this;
     }
+
+
 }
