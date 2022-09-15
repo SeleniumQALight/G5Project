ec8149097267364c0e8f4eb057c696cc91d7f7f2
KostiushkoDenis <kostiushko.denis@gmail.com>
2022-09-15 21:19:01 +0300

    Kostiushko-09-15  3

--- kostiushkoProj/src/main/java/pages/CommonActionsWithElements.java
+++ kostiushkoProj/src/main/java/pages/CommonActionsWithElements.java
@@ -1,5 +1,7 @@
 package pages;
 
+import libs.ConfigProperties;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.*;
@@ -15,13 +17,14 @@ import java.util.ArrayList;
 public class CommonActionsWithElements {
     protected WebDriver webDriver;
     Logger logger = Logger.getLogger(getClass());
-    protected WebDriverWait webDriverWait10, webDriverWait15;
+    protected WebDriverWait webDriverWaitLow, webDriverWaitHight;
+    public static ConfigProperties configProperties = ConfigFactory.create(ConfigProperties.class);
 
     public CommonActionsWithElements(WebDriver webDriver) {
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this);
-        webDriverWait10 = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-        webDriverWait15 = new WebDriverWait(webDriver, Duration.ofSeconds(15));
+        webDriverWaitLow = new WebDriverWait(webDriver, Duration.ofSeconds(configProperties.TIME_FOR_EXPLICIT_WAIT_LOW()));
+        webDriverWaitHight = new WebDriverWait(webDriver, Duration.ofSeconds(configProperties.TIME_FOR_EXPLICIT_WAIT_HIGHT()));
     }
 
 
@@ -29,7 +32,7 @@ public class CommonActionsWithElements {
         try {
             webElement.clear();
             webElement.sendKeys(text);
-            logger.info("'" + text + "'was input into " + webElement.getAccessibleName() + "'");
+            logger.info("'" + text + "'was input into " + getElementName(webElement) + "'");
         } catch (Exception e) {
             prinErrorAndStopTest(e);
         }
@@ -37,8 +40,8 @@ public class CommonActionsWithElements {
 
     protected void clicOnElement(WebElement webElement) {
         try {
-            webDriverWait15.until(ExpectedConditions.elementToBeClickable(webElement));
-            String name = webElement.getAccessibleName();
+            webDriverWaitHight.until(ExpectedConditions.elementToBeClickable(webElement));
+            String name = getElementName(webElement);
             webElement.click();
             logger.info("'" + name + " ' was clicked");
         } catch (Exception e) {
@@ -172,5 +175,13 @@ public class CommonActionsWithElements {
 //            webElement = driver.findElement(By.xpath("bla-bla-bla"));
 //((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", webElement);
 
+
+    private String getElementName (WebElement webElement){
+        try {return webElement.getAccessibleName();
+
+        }catch (Exception e){
+            return "";
+        }
+    }
 
 }
