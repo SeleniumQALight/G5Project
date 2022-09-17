83dcaf791bc2f08c18b5b68969605debc4760d9f
boiaryntseva <boiaryntseva@gmail.com>
2022-09-17 10:44:07 -0400

    Valid login using keyboard

--- boiaryntsevaProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ boiaryntsevaProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -26,6 +26,18 @@ public class LoginTestWithPageObject extends BaseTest {
     }
 
     @Test
+    public void validLoginUsingKeyboard(){
+        loginPage.openLoginPage();
+        loginPage.enterUserNameIntoLoginInput(TestData.VALID_LOGIN);
+        loginPage.usersPressesKeyTabTime(1);
+        loginPage.enterPasswordIntoPasswordInput(TestData.VALID_PASSWORD);
+        loginPage.usersPressesKeyEnterTime(1);
+
+        Assert.assertTrue("Sign out button is not displayed", headerElement.isButtonSignedOutDisplayed());
+
+    }
+
+    @Test
     @Parameters
             ({
                     TestData.VALID_LOGIN+COMMA+"123"
