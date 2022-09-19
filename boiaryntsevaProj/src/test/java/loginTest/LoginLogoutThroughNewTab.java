d47cc7d3c4fe1264ad48376a55bf0bcc102a8fb1
boiaryntseva <boiaryntseva@gmail.com>
2022-09-19 09:47:42 -0400

    Log out test

--- boiaryntsevaProj/src/test/java/loginTest/LoginLogoutThroughNewTab.java
+++ boiaryntsevaProj/src/test/java/loginTest/LoginLogoutThroughNewTab.java
@@ -2,11 +2,12 @@ package loginTest;
 
 import baseTest.BaseTest;
 import libs.TestData;
+import libs.Util;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class LoginNewTab extends BaseTest {
+public class LoginLogoutThroughNewTab extends BaseTest {
 
     @Before
     public void validLogin() {
@@ -28,7 +29,19 @@ public class LoginNewTab extends BaseTest {
         Assert.assertTrue("Sign out button is not displayed", headerElement.isButtonSignedOutDisplayed());
         Assert.assertEquals("Different name is displayed", TestData.VALID_LOGIN, loginPage.getTextLoggedInUserNameAsString());
 
+    }
 
+    @Test
+    public void loggedOutUserRemainsLoggedOutIneNewTab(){
+        homePage.userOpensNewTab();
+        loginPage.openLoginPage();
+        loginPage.userSwitchesToParentWindow();
+        homePage.getHeaderElement().clickOnSignOutButton();
+        Assert.assertFalse("Sign out button is displayed", headerElement.isButtonSignedOutDisplayed());
+
+        homePage.userSwitchesToNextWindowTab();
+        homePage.refreshPage();
+        Assert.assertFalse("Sign out button is displayed", headerElement.isButtonSignedOutDisplayed());
 
     }
 }
