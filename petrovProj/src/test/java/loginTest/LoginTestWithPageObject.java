0f2c2e67946dff00ba051237fdec6f69b977d92a
Andrey Petrov <psandriy@ukr.net>
2022-09-26 16:11:40 +0300

    HW 7 add CreateNewPostWithDbDataTest

--- petrovProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ petrovProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -4,7 +4,6 @@ import baseTest.BaseTest;
 import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
-import pages.CommonActionsWithElements;
 
 import java.io.IOException;
 import java.util.Map;
