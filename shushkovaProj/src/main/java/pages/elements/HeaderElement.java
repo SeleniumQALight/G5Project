ee98d5150fa946fb956a5ffd1d806e3a11a742c0
shushkovaira <shushkovaira@gmail.com>
2022-09-27 21:53:00 +0300

    Suits with categories+allure

--- shushkovaProj/src/main/java/pages/elements/HeaderElement.java
+++ shushkovaProj/src/main/java/pages/elements/HeaderElement.java
@@ -1,5 +1,6 @@
 package pages.elements;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -32,6 +33,7 @@ public class HeaderElement extends CommonActionsWithElements {
         return new CreatePostPage(webDriver);
 
     }
+    @Step
     public boolean isButtonSingOutDisplayed(){
             return isElementDisplayed(buttonSingOut);
         }
--- shushkovaProj/src/test/java/baseTest/BaseTest.java
+++ shushkovaProj/src/test/java/baseTest/BaseTest.java
@@ -2,11 +2,16 @@ package baseTest;
 
 import java.time.Duration;
 
+import io.qameta.allure.Attachment;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -39,14 +44,45 @@ public class BaseTest {
         myProfilePage=new MyProfilePage(webDriver);
         editPage=new EditPage(webDriver);
     }
-    @After
-    public void tearDown(){
-        webDriver.quit();
-        logger.info("Browser was closed");
-        logger.info("------"+testName.getMethodName()+" was ended-------");
-    }
+ //   @After
+ //   public void tearDown(){
+ //       webDriver.quit();
+ //       logger.info("Browser was closed");
+ //       logger.info("------"+testName.getMethodName()+" was ended-------");
+ //   }
+
     @Rule
     public TestName testName=new TestName();
+
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            screenshot();
+        }
+        @Attachment(value = "Page screenshot", type = "image/png")
+        public byte[] saveScreenshot(byte[] screenShot) {
+            return screenShot;
+        }
+        public void screenshot() {
+            if (webDriver == null) {
+                logger.info("Driver for screenshot not found");
+                return;
+            }
+            saveScreenshot(((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES));
+        }
+        @Override
+        protected void finished(Description description) {
+            logger.info(String.format("Finished test: %s::%s", description.getClassName(), description.getMethodName()));
+            try {
+                webDriver.quit();
+                logger.info("Browser was closed");
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        }
+    };
+
 
     private WebDriver initDriver(){
         String browser=System.getProperty("browser");
