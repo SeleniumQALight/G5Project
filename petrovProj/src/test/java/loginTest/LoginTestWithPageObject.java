23c2bf8c56bc941b1e68c00975bd3eb18287afae
Andrey Petrov <psandriy@ukr.net>
2022-09-27 22:30:25 +0300

    allure

--- petrovProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ petrovProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -2,6 +2,7 @@ package loginTest;
 
 import baseTest.BaseTest;
 import categories.SmokeTestFilter;
+import io.qameta.allure.*;
 import libs.ExcelDriver;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,13 +13,22 @@ import java.util.Map;
 
 import static pages.CommonActionsWithElements.configProperties;
 
+@Epic("Allure examples")
+@Feature("Junit 4 support")
 public class LoginTestWithPageObject extends BaseTest {
 
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Base support for bdd annotations")
     @Test
     @Category(SmokeTestFilter.class)
     public void validLogin(){
         loginPage.openLoginPage();
-        loginPage.enterUserNameIntoLoginInput("qaauto");
+        loginPage.enterUserNameIntoLoginInput("qaauto0");
         loginPage.enterPasswordIntoInputPassword("123456qwerty");
         loginPage.clickOnButtonLogin();
         Assert.assertTrue("Button Sign Out is not visible", homePage.isButtonSignOutDisplayed());
