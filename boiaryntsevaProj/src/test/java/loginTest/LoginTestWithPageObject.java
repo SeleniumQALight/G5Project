9378c2f48a2c2f2de45a7243d73701617b619fd7
boiaryntseva <boiaryntseva@gmail.com>
2022-09-27 12:35:26 -0400

    Categories are added

--- boiaryntsevaProj/src/test/java/loginTest/LoginTestWithPageObject.java
+++ boiaryntsevaProj/src/test/java/loginTest/LoginTestWithPageObject.java
@@ -1,6 +1,7 @@
 package loginTest;
 
 import baseTest.BaseTest;
+import categories.SmokeTestFilter;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import junitparams.naming.TestCaseName;
@@ -8,16 +9,18 @@ import libs.ExcelDriver;
 import libs.TestData;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import pages.CommonActionsWithElements;
 
 import java.io.IOException;
 import java.util.Map;
 
-@RunWith(JUnitParamsRunner.class)
+//@RunWith(JUnitParamsRunner.class)
 public class LoginTestWithPageObject extends BaseTest {
     final static String COMMA = ",";
     @Test
+    @Category(SmokeTestFilter.class)
     public void validLogin() {
         loginPage.openLoginPage();
         loginPage.enterUserNameIntoLoginInput("qaauto");
@@ -40,22 +43,22 @@ public class LoginTestWithPageObject extends BaseTest {
 
     }
 
-    @Test
-    @Parameters
-            ({
-                    TestData.VALID_LOGIN+COMMA+"123"
-                    ,"tr"+COMMA+TestData.VALID_PASSWORD
-                    ,"tr"+COMMA+"123"
-            })
-    @TestCaseName("login errors: login{0}, password{1}")
-    public void invalidLogin(String username, String password) {
-        loginPage.openLoginPage();
-        loginPage.enterUserNameIntoLoginInput(username);
-        loginPage.enterPasswordIntoPasswordInput(password);
-        loginPage.clickOnButtonLogin();
-
-        Assert.assertFalse("Sign out button is not displayed", headerElement.isButtonSignedOutDisplayed());
-        Assert.assertTrue("Invalid login message is not visible", loginPage.isInvalidLoginMsgVisible());
-
-    }
+//    @Test
+//    @Parameters
+//            ({
+//                    TestData.VALID_LOGIN+COMMA+"123"
+//                    ,"tr"+COMMA+TestData.VALID_PASSWORD
+//                    ,"tr"+COMMA+"123"
+//            })
+//    @TestCaseName("login errors: login{0}, password{1}")
+//    public void invalidLogin(String username, String password) {
+//        loginPage.openLoginPage();
+//        loginPage.enterUserNameIntoLoginInput(username);
+//        loginPage.enterPasswordIntoPasswordInput(password);
+//        loginPage.clickOnButtonLogin();
+//
+//        Assert.assertFalse("Sign out button is not displayed", headerElement.isButtonSignedOutDisplayed());
+//        Assert.assertTrue("Invalid login message is not visible", loginPage.isInvalidLoginMsgVisible());
+//
+//    }
 }
