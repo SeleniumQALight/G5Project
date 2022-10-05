1cf82bc7b1440815aaa8ac07df268c7ae65e3cd9
likakniazieva <likakniazieva@gmail.com>
2022-10-05 16:36:50 +0200

    methods

--- kniazievaProj/src/main/java/pages/LoginPage.java
+++ kniazievaProj/src/main/java/pages/LoginPage.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
