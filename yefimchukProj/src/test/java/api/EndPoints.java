5378f117d9656e1c2114a7aafabb29687ce4f067
VYefimchuk <Klunenko.Vika@gmail.com>
2022-10-11 21:54:40 +0300

    API test

---
+++ yefimchukProj/src/test/java/api/EndPoints.java
@@ -0,0 +1,6 @@
+package api;
+
+public interface EndPoints {
+    String baseUrl = "http://qa-complex-app-for-testing.herokuapp.com";
+    String POST_BY_USER = baseUrl + "/api/postsByAuthor/{0}";
+}
