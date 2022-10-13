0605942c10755e5aeb3c82fddf9b1b379ddb0d2e
boiaryntseva <boiaryntseva@gmail.com>
2022-10-13 14:58:01 -0400

    Api test body validation

--- boiaryntsevaProj/src/test/java/apiTests/ApiTests.java
+++ boiaryntsevaProj/src/test/java/apiTests/ApiTests.java
@@ -1,7 +1,12 @@
 package apiTests;
 
+import api.AuthorDTO;
 import api.EndPoints;
+import api.PostDTO;
 import io.restassured.http.ContentType;
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static api.EndPoints.POST_BY_USER;
@@ -10,17 +15,44 @@ import static io.restassured.RestAssured.given;
 
 public class ApiTests {
     String userName="autoapi";
+    Logger logger = Logger.getLogger(getClass());
 
     @Test
-    public void getAllPostsByUser(){
-        given()
-                    .contentType(ContentType.JSON)
-                    .log().all()
+    public void getAllPostsByUser() {
+        PostDTO[] responseBody = given()
+                .contentType(ContentType.JSON)
+                .log().all()
                 .when()
-                    .get(POST_BY_USER, userName)
+                .get(POST_BY_USER, userName)
                 .then()
-                    .statusCode(200)
-                    .log().all()
-                    ;
+                .statusCode(200)
+                .log().all()
+                .extract()
+                .response().as(PostDTO[].class);
+
+        logger.info("Number of posts is " + responseBody.length);
+        logger.info("Post title of post no 1: " + responseBody[0].getTitle());
+        logger.info("Username : " + responseBody[0].getAuthor().getUsername());
+
+        for (int i = 0; i < responseBody.length; i++) {
+            Assert.assertEquals("Username is not matching", userName, responseBody[i].getAuthor().getUsername());
+        }
+
+        PostDTO[] expectedResult = {
+                new PostDTO("test", "test body","All Users", "no", new AuthorDTO("autoapi"),false),
+                new PostDTO("test2", "test body2","All Users", "no", new AuthorDTO("autoapi"),false)
+        };
+
+        Assert.assertEquals(expectedResult.length, responseBody.length );
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        for (int i = 0; i < expectedResult.length; i++) {
+            softAssertions.assertThat(responseBody[i])
+                    .isEqualToIgnoringGivenFields(expectedResult[i], "id", "createdDate", "author");
+//            softAssertions.assertThat(responseBody[i].getAuthor())
+//                    .isEqualToIgnoringGivenFields(expectedResult[i].getAuthor(), "avatar");
+
+            softAssertions.assertAll();
+        }
     }
 }
