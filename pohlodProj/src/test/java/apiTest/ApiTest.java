72545faee8ad6513aebc39ad143b8e5b1b41af01
Iryna <pohlodiryna@gmail.com>
2022-10-18 21:16:38 +0300

    pohlod-10-18 json Schema

--- pohlodProj/src/test/java/apiTest/ApiTest.java
+++ pohlodProj/src/test/java/apiTest/ApiTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 
 public class ApiTest {
@@ -112,6 +113,20 @@ public class ApiTest {
 
 
         softAssertions.assertAll();
+
+    }
+
+    @Test
+    public void getAllPostsByUserSchema() {
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(EndPoints.POST_BY_USER, userName)
+                        .then()
+                        .statusCode(200)
+                        .log().all()
+                        .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
 
     }
 
