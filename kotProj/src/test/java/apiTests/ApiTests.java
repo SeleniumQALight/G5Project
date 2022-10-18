a5c3116a6aeb254aa4188f26393d1a52fde9b65e
VictoriaKot <vkot4447@gmail.com>
2022-10-18 21:19:59 +0300

    Kot-schemaValidator

--- kotProj/src/test/java/apiTests/ApiTests.java
+++ kotProj/src/test/java/apiTests/ApiTests.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class ApiTests {
     String userName = "autoapi";
@@ -112,7 +113,18 @@ Assert.assertEquals("Number of posts does not equal", expectedResult.length, res
 
     }
 
-
+@Test
+    public void getAllPostsByUserSchema (){
+        given()
+                .contentType(ContentType.JSON)
+                .log().all()
+           .when()
+                .get(EndPoints.POST_BY_USER,userName)
+            .then()
+                .statusCode(200)
+                .log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("response.json"));
+}
 
 
 
