ab76d081259b6691229afbe5e50dc987cbbe4b33
boiaryntseva <boiaryntseva@gmail.com>
2022-10-18 12:39:42 -0400

    Lombok

--- boiaryntsevaProj/src/main/java/api/AuthorDTO.java
+++ boiaryntsevaProj/src/main/java/api/AuthorDTO.java
@@ -1,38 +1,47 @@
 package api;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
 public class AuthorDTO {
 
-    public AuthorDTO(String username) {
-        this.username = username;
-    }
+//    public AuthorDTO(String username) {
+//        this.username = username;
+//    }
 
     String username;
     String avatar;
 
-    public AuthorDTO() {
-    }
+//    public AuthorDTO() {
+//    }
 
-    public String getUsername() {
-        return username;
-    }
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getAvatar() {
+//        return avatar;
+//    }
+//
+//    public void setAvatar(String avatar) {
+//        this.avatar = avatar;
+//    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    @Override
-    public String toString() {
-        return "AuthorDTO{" +
-                "username='" + username + '\'' +
-                ", avatar='" + avatar + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "AuthorDTO{" +
+//                "username='" + username + '\'' +
+//                ", avatar='" + avatar + '\'' +
+//                '}';
+//    }
 }
