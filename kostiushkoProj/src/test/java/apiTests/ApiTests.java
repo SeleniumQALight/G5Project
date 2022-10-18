58648f41a7091753284e385c6bb02f7fb94a271c
KostiushkoDenis <kostiushko.denis@gmail.com>
2022-10-18 20:01:19 +0300

    Kostiushko respons String

--- kostiushkoProj/src/test/java/apiTests/ApiTests.java
+++ kostiushkoProj/src/test/java/apiTests/ApiTests.java
@@ -54,4 +54,18 @@ PostDTO[] expectedResult = {
 
         softAssertions.assertAll();
     }
+
+    @Test
+    public void getAllPostByUserNegative(){
+        String actualResponse =
+                given().contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(EndPoints.POST_BY_USER, "notValidUser")
+                        .then().statusCode(200)
+                        .log().all()
+                        .extract().response().getBody().asString();
+        Assert.assertEquals("Message in response ","\"Sorry, invalid user requested.undefined\"", actualResponse);
+        Assert.assertEquals("Message in response ","Sorry, invalid user requested.undefined", actualResponse.replace("\"",""));
+    }
 }
