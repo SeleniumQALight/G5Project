9afb8d00034ef6151dd47983512f71eb9dcd8c40
Ndividual17 <ndividualkim17@gmail.com>
2022-10-20 00:03:45 +0300

    created ApiTestsForPrivatBank, EndPointsForPrivatBank, ExchangeCourseResponseFromPrivatBankDTO for HW#1

---
+++ yakymenkoProj/src/main/java/api/ExchangeCourseResponseFromPrivatBankDTO.java
@@ -0,0 +1,58 @@
+package api;
+
+public class ExchangeCourseResponseFromPrivatBankDTO {
+    String ccy;
+    String base_ccy;
+    String buy;
+    String sale;
+
+    public ExchangeCourseResponseFromPrivatBankDTO() {
+    }
+
+    public ExchangeCourseResponseFromPrivatBankDTO(String ccy, String base_ccy) {
+        this.ccy = ccy;
+        this.base_ccy = base_ccy;
+    }
+
+    public String getCcy() {
+        return ccy;
+    }
+
+    public void setCcy(String ccy) {
+        this.ccy = ccy;
+    }
+
+    public String getBase_ccy() {
+        return base_ccy;
+    }
+
+    public void setBase_ccy(String base_ccy) {
+        this.base_ccy = base_ccy;
+    }
+
+    public String getBuy() {
+        return buy;
+    }
+
+    public void setBuy(String buy) {
+        this.buy = buy;
+    }
+
+    public String getSale() {
+        return sale;
+    }
+
+    public void setSale(String sale) {
+        this.sale = sale;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeCourseResponseFromPrivatBankDTO{" +
+                "ccy='" + ccy + '\'' +
+                ", base_ccy='" + base_ccy + '\'' +
+                ", buy='" + buy + '\'' +
+                ", sale='" + sale + '\'' +
+                '}';
+    }
+}
