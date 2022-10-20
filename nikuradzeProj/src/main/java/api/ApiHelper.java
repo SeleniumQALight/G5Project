951ebe3014ba317b6f5c4ca05737991f77bc64a6
nikuradze <sergey.nikuradze@gmail.com>
2022-10-20 21:08:56 +0300

    CreatePostByApi

---
+++ nikuradzeProj/src/main/java/api/ApiHelper.java
@@ -0,0 +1,72 @@
+package api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiHelper {
+    public static final String USER_NAME = "qasnik";
+    private final String PASSWORD = "123456qwerty";
+
+    RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+
+    /**
+     * Get token for default user
+     * @return
+     */
+    public String getToken() {
+        return getToken(USER_NAME, PASSWORD);
+    }
+
+    public String getToken(String user_name, String password) {
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("username", user_name);
+        requestParams.put("password", password);
+
+        ResponseBody responseBody =
+                given()
+/*                        .contentType(ContentType.JSON)
+                        .log().all()*/
+                        .spec(requestSpecification)
+                        .body(requestParams.toMap())
+                .when()
+                        .post(EndPoints.LOGIN)
+                .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response().getBody();
+
+        return responseBody.asString().replace("\"","");
+
+    }
+
+    /**
+     * Get posts for DEFAULT user
+     * @return
+     */
+    public PostDTO[] getAllPostsByUser() {
+        return getAllPostsByUser(USER_NAME);
+
+
+    }
+
+    private PostDTO[] getAllPostsByUser(String user_name) {
+        return given()
+                  .spec(requestSpecification)
+               .when()
+                  .get(EndPoints.POST_BY_USER, user_name)
+               .then()
+                  .statusCode(200)
+                  .log().all()
+                  .extract().response().getBody().as(PostDTO[].class);
+    }
+}
