fcb03bcfbcc147a223d48e3710b11819020570f6
Iryna <pohlodiryna@gmail.com>
2022-10-20 21:08:14 +0300

    pohlod-10-20 GetPostsApi

---
+++ pohlodProj/src/main/java/api/ApiHelper.java
@@ -0,0 +1,74 @@
+package api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiHelper {
+
+    public static String USER_NAME = "nick2022";
+    private final String PASSWORD = "PasswordNick2022";
+
+    RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    /**
+     * Get token to default user
+     * @return
+     */
+
+
+    public String getToken(){
+        return getToken(USER_NAME, PASSWORD);
+    }
+
+    public String getToken(String user_name, String password) {
+        JSONObject requestParam = new JSONObject();
+        requestParam.put("username", user_name);
+        requestParam.put("password", password);
+
+        ResponseBody responseBody =
+                given()
+                       /* .contentType(ContentType.JSON)
+                        .log().all()*/
+                        .spec(requestSpecification)
+                        .body(requestParam.toMap())
+                .when()
+                        .post(EndPoints.LOGIN)
+                .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response().getBody();
+
+        return responseBody.asString();
+
+    }
+
+    /**
+     * Get POsts for default user
+     * @return
+     */
+    public PostDTO[] getAllPostsByUser() {
+        return getAllPostsByUser(USER_NAME);
+    }
+
+    private PostDTO[] getAllPostsByUser(String user_name) {
+        return given()
+                .spec(requestSpecification)
+        .when()
+                .get(EndPoints.POST_BY_USER,user_name)
+        .then()
+                .statusCode(200)
+                .log().all()
+                .extract().response().getBody().as(PostDTO[].class);
+    }
+
+
+}
---
+++ pohlodProj/src/test/java/apiTest/CreatePostByApi.java
@@ -0,0 +1,70 @@
+package apiTest;
+
+import api.ApiHelper;
+import api.AuthorDTO;
+import api.EndPoints;
+import api.PostDTO;
+import io.restassured.http.ContentType;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.requestSpecification;
+
+public class CreatePostByApi  {
+        ApiHelper apiHelper = new ApiHelper();
+
+        @Test
+        public void createPostByApi(){
+            String token = apiHelper.getToken().replace("\"","");
+    //        System.out.println("token from test - " + token);
+
+            HashMap<String, String> requestParams = new HashMap<>();
+            requestParams.put("title", "New post from Api");
+            requestParams.put("body","post body");
+            requestParams.put("select1", "One Person");
+            requestParams.put("uniquePost", "yes");
+            requestParams.put("token", token);
+
+            String response =
+                    given()
+                  .contentType(ContentType.JSON)
+                            .log().all()
+                            .body(requestParams)
+            .when()
+                             .post(EndPoints.CREATE_POST)
+            .then()
+                              .statusCode(200)
+                               .log().all()
+                              .extract().response().getBody().asString();
+
+            Assert.assertEquals("Message ", "\"Congrats.\"", response);
+
+            PostDTO[] actualPostDTO = apiHelper.getAllPostsByUser();
+            Assert.assertEquals("Number of posts ", 1, actualPostDTO.length);
+
+            PostDTO[] expectedPostDTO = {
+                    PostDTO.builder()
+                            .title(requestParams.get("title"))
+                            .body(requestParams.get("body"))
+                            .select1(requestParams.get("select1"))
+                            .uniquePost(requestParams.get("uniquePost"))
+                            .author(AuthorDTO.builder().username(ApiHelper.USER_NAME).build())
+                            .isVisitorOwner(false)
+                            .build()
+            };
+
+            SoftAssertions softAssertions = new SoftAssertions();
+            softAssertions.assertThat(actualPostDTO[0])
+                            .isEqualToIgnoringGivenFields(expectedPostDTO[0],"id","createdDate","author");
+            softAssertions.assertThat(actualPostDTO[0].getAuthor())
+                    .isEqualToIgnoringGivenFields(expectedPostDTO[0].getAuthor(),"avatar");
+            softAssertions.assertAll();
+        }
+
+    }
+
+
