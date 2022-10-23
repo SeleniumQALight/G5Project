e32da9ccbe41e5faf1098bd76eb4f3538cf6963a
nikuradze <sergey.nikuradze@gmail.com>
2022-10-23 17:16:54 +0300

    AddBookToUserByApi

---
+++ nikuradzeProj/src/test/java/apiTests/AddBookToUserByApi.java
@@ -0,0 +1,33 @@
+package apiTests;
+
+import api.ApiHelperBooks;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AddBookToUserByApi {
+    Logger logger = Logger.getLogger(getClass());
+    ApiHelperBooks apiHelperBooks = new ApiHelperBooks();
+
+    @Before
+    public void deleteAllBooksForUser(){
+        apiHelperBooks.login();
+        apiHelperBooks.deleteBooks();
+    }
+
+    @Test
+    public void addBookToUser(){
+        apiHelperBooks.getAllBooksFromStore();
+        String firstBookIsbn = apiHelperBooks.getAllBooksFromStore().getBooks()[0].getIsbn();
+
+        apiHelperBooks.addBooksToUser(firstBookIsbn);
+
+        apiHelperBooks.getAllBooksByUser();
+
+        Assert.assertTrue("Number of books added by user is ", (apiHelperBooks.getAllBooksByUser().getBooks().length==1)
+                && (apiHelperBooks.getAllBooksByUser().getBooks()[0].getIsbn().equals(firstBookIsbn)));
+        logger.info(String.format("Book with isbn %s is placed in user's library", firstBookIsbn));
+    }
+}
