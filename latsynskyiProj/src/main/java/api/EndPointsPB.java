d5943943a4a58cc57f206c1cd8a4f4ff6dd4c4d1
Anto <latsynskyi@gmail.com>
2022-10-24 01:31:53 +0300

    HW1

---
+++ latsynskyiProj/src/main/java/api/EndPointsPB.java
@@ -0,0 +1,7 @@
+package api;
+
+public interface EndPointsPB {
+    String baseUrlPB = "https://api.privatbank.ua";
+    String GET_FOR_CURRENCY = baseUrlPB + "/p24api/pubinfo?exchange&json&coursid={0}";
+
+}
---
+++ latsynskyiProj/src/main/java/api/GetDTOPB.java
@@ -0,0 +1,63 @@
+package api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GetDTOPB {
+    @JsonProperty
+
+    String ccy ;
+    String base_ccy;
+    Double buy;
+    Double sale;
+
+    public GetDTOPB (){
+
+    }
+
+    public GetDTOPB(String ccy, String base_ccy) {
+        this.ccy = ccy;
+        this.base_ccy = base_ccy;
+    }
+
+    public String getCcy() {
+        return ccy;
+    }
+
+    public void setCcy(String ccy) {
+        this.ccy = ccy;
+    }
+
+    public String getBase_ccy() {
+        return base_ccy;
+    }
+
+    public void setBase_ccy(String base_ccy) {
+        this.base_ccy = base_ccy;
+    }
+
+    public Double getBuy() {
+        return buy;
+    }
+
+    public void setBuy(Double buy) {
+        this.buy = buy;
+    }
+
+    public Double getSale() {
+        return sale;
+    }
+
+    public void setSale(Double sale) {
+        this.sale = sale;
+    }
+
+    @Override
+    public String toString() {
+        return "GetDTOPB{" +
+                "ccy='" + ccy + '\'' +
+                ", base_ccy='" + base_ccy + '\'' +
+                ", buy=" + buy +
+                ", sale=" + sale +
+                '}';
+    }
+}
