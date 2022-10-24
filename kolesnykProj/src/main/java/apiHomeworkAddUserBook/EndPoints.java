6e5929b1bb3d4460f32ed7ab1fdc864a1caab58c
RossKoll <rosko48@gmail.com>
2022-10-24 19:47:30 +0300

    #2 : removed duplicated endpoin from EndPoints

--- kolesnykProj/src/main/java/apiHomeworkAddUserBook/EndPoints.java
+++ kolesnykProj/src/main/java/apiHomeworkAddUserBook/EndPoints.java
@@ -4,6 +4,5 @@ public interface EndPoints {
     String baseUrl = "https://demoqa.com";
     String loginUrl = baseUrl + "/Account/v1/Login";
     String deleteUserBook = baseUrl + "/BookStore/v1/Books";
-    String getAllBooks = baseUrl + "/BookStore/v1/Books";
     String getUserBooks = baseUrl + "/Account/v1/User/{0}";
 }
