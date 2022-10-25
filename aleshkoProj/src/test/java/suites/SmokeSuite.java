bd073d0d7685745dedaaa67419b9117ff2df0469
Aleshko_Kyryl <aleshko.k@gmail.com>
2022-10-25 20:00:02 +0300

    Allure

--- aleshkoProj/src/test/java/suites/SmokeSuite.java
+++ aleshkoProj/src/test/java/suites/SmokeSuite.java
@@ -1,5 +1,6 @@
 package suites;
 
+import apiTests.ApiTests;
 import loginTest.LoginTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -8,7 +9,8 @@ import postsTest.CreatePostTest;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         LoginTest.class,
-        CreatePostTest.class
+        CreatePostTest.class,
+        ApiTests.class
 })
 public class SmokeSuite {
 
