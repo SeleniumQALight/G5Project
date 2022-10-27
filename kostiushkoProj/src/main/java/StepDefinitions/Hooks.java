28b509b5ddb8b1166a511958c285d5b27cc6f690
KostiushkoDenis <kostiushko.denis@gmail.com>
2022-10-27 21:26:10 +0300

    Kostiushko 2

--- kostiushkoProj/src/main/java/StepDefinitions/Hooks.java
+++ kostiushkoProj/src/main/java/StepDefinitions/Hooks.java
@@ -1,19 +1,25 @@
 package StepDefinitions;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import libs.DriverHelper;
+import org.apache.log4j.Logger;
 
 public class Hooks {
 DriverHelper driverHelper = new DriverHelper();
-    @Before
-    public void setUp(){
-driverHelper.createWebDriver();
+Logger logger = Logger.getLogger(getClass());
+
+@Before
+    public void setUp(Scenario scenario){
+    driverHelper.createWebDriver();
+    logger.info(scenario.getName() + " was started");
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
 driverHelper.closeWebDriver();
+        logger.info(scenario.getName() + " was ended wis status" + scenario.getStatus());
     }
 
 }
