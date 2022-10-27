db216dd54b6760647f0678dff8174ce34a6bbaa9
viktor.loboda <valksua@gmail.com>
2022-10-27 21:14:12 +0300

    TC1 cucumber

---
+++ lobodaProj/src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,23 @@
+package StepDefinitions;
+
+
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import libs.DriverHelper;
+
+public class Hooks {
+
+    DriverHelper driverHelper = new DriverHelper();
+    @Before
+    public void setUp(){
+        driverHelper.createWebDriver();
+
+    }
+
+    @After
+    public void tearDown(){
+        driverHelper.closeWebDriver();
+
+    }
+}
