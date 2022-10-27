725ad796a91d15a58ec807ef3ca76b2777729911
Taras Radulenko <Taras_Radulenko@epam.com>
2022-10-27 21:25:17 +0300

    TC1 cucumber part2

--- overAll/src/test/java/StepDefinitions/Hooks.java
+++ overAll/src/test/java/StepDefinitions/Hooks.java
@@ -1,18 +1,25 @@
 package StepDefinitions;
 
+import org.apache.log4j.Logger;
+
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import libs.DriverHelper;
 
 public class Hooks {
     DriverHelper driverHelper = new DriverHelper();
+    Logger logger = Logger.getLogger(getClass());
+
     @Before
-    public void setUp(){
+    public void setUp(Scenario scenario){
         driverHelper.createWebDriver();
+        logger.info(scenario.getName() + " was started");
     }
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
         driverHelper.closeWebDriver();
+        logger.info(scenario.getName() + " was ended with status " + scenario.getStatus());
     }
 
 }
