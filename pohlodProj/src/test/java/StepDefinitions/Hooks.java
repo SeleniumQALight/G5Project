89c9a2bc5b199f42ee57a16783e6c8d3c32ed5ce
Iryna <pohlodiryna@gmail.com>
2022-10-30 18:05:59 +0200

    pohlod-10-27 TestCase1:: part2

--- pohlodProj/src/test/java/StepDefinitions/Hooks.java
+++ pohlodProj/src/test/java/StepDefinitions/Hooks.java
@@ -1,21 +1,26 @@
 package StepDefinitions;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import libs.DriverHelper;
+import org.apache.log4j.Logger;
 
 public class Hooks {
 
     DriverHelper driverHelper = new DriverHelper();
+    Logger logger = Logger.getLogger(getClass());
 
     @Before
-    public void setUp(){
+    public void setUp(Scenario scenario){
         driverHelper.createDriver();
+        logger.info(scenario.getName() + " was started");
     }
 
     @After
-    public void tearDown(){
+    public void tearDown(Scenario scenario){
         driverHelper.closeWebDriver();
+        logger.info(scenario.getName() + " was ended with status " + scenario.getStatus());
     }
 
 
