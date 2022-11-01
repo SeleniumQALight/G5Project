779d94b746d54884b016656a728025afd6dde14b
Yuriy-Kuschch <kushchuriy@gmail.com>
2022-11-01 21:48:32 +0200

    cucumber 1-11

---
+++ kuschchProj/src/test/java/StepDefinitions/API_StepDefinition.java
@@ -0,0 +1,27 @@
+package StepDefinitions;
+
+
+import api.ApiHelper;
+import cucumber.api.java.en.Given;
+import libs.TestData;
+
+
+public class API_StepDefinition {
+    final String DEFAULT = "default";
+
+    ApiHelper apiHelper = new ApiHelper();
+
+    @Given("^Crreate (\\d+) new posts via API for '(.*)' user and '(.*)' password$")
+    public void createNewPostsApiForUser(int numberOfPosts, String userName, String password){
+        if (DEFAULT.equalsIgnoreCase(userName)){
+            userName = TestData.VALID_LOGIN;
+        }
+        if (DEFAULT.equalsIgnoreCase(password)){
+            password = TestData.VALID_PASSWORD;
+        }
+        for (int i = 0; i < numberOfPosts; i++) {
+            apiHelper.createPost("Post from APi " + i, userName, password);
+        }
+
+    }
+}
