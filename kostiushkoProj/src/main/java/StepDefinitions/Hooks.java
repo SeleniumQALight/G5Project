fa3568d545ff0570a1f117138f8666defacb3f21
KostiushkoDenis <kostiushko.denis@gmail.com>
2022-11-01 21:40:20 +0200

    Kostiushko

--- kostiushkoProj/src/main/java/StepDefinitions/Hooks.java
+++ kostiushkoProj/src/main/java/StepDefinitions/Hooks.java
@@ -1,25 +1,34 @@
 package StepDefinitions;
 
+import api.ApiHelper;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import libs.DriverHelper;
+import libs.TestData;
 import org.apache.log4j.Logger;
 
 public class Hooks {
 DriverHelper driverHelper = new DriverHelper();
 Logger logger = Logger.getLogger(getClass());
+ApiHelper apiHelper = new ApiHelper();
 
-@Before
+@Before (order = 0)
     public void setUp(Scenario scenario){
     driverHelper.createWebDriver();
     logger.info(scenario.getName() + " was started");
     }
 
-    @After
+    @After (order = 0)
     public void tearDown(Scenario scenario){
 driverHelper.closeWebDriver();
         logger.info(scenario.getName() + " was ended wis status" + scenario.getStatus());
+    }
+
+    @Before(value = "@BeforeDeletingAllPostsForDefaultUser", order = 100)
+    @After(value = "@AfterDeletingAllPostsForDefaultUser", order = 50)
+public void deletingAllPostsForDefaultUser (){
+apiHelper.deletePostsTillPresent(TestData.VALID_LOGIN, TestData.VALID_PASSWORD);
     }
 
 }
--- kostiushkoProj/src/main/java/libs/TestData.java
+++ kostiushkoProj/src/main/java/libs/TestData.java
@@ -2,8 +2,8 @@ package libs;
 
 public class TestData {
 
-    public final static String VALID_LOGIN = "qaauto";
-    public final static String VALID_PASSWORD = "123456qwerty";
+    public final static String VALID_LOGIN = "Kostiushko1";
+    public final static String VALID_PASSWORD = "Filinoertii1991a";
 
 
 }
