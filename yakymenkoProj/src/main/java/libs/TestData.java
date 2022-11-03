df5f76832c1e0f96f5523c4af4609380380f120f
Ndividual17 <ndividualkim17@gmail.com>
2022-11-03 08:44:32 +0200

    TC11 cucumber + api

--- yakymenkoProj/src/main/java/libs/TestData.java
+++ yakymenkoProj/src/main/java/libs/TestData.java
@@ -4,6 +4,6 @@ package libs;
  * Клас, дле ми будемо зберігати ті тестові дані де вони будуть використовуватись у багатьох місцях
  */
 public class TestData {
-    public final static String VALID_LOGIN = "qaauto"; // якщо final(константа -  незмінна) → CAPS_LOCK
-    public final static String VALID_PASSWORD = "123456qwerty";// static - доступ до неї можемо отримати через ім'я класу(TestData) і змінної(VALID_PASSWORD) в будь-якому місці програми (тому що створюється спочатку і займає місце поки наша програма виконується)
+    public final static String VALID_LOGIN = "oleksandr"; // "qaauto" якщо final(константа -  незмінна) → CAPS_LOCK
+    public final static String VALID_PASSWORD = "Password0000";// "123456qwerty" static - доступ до неї можемо отримати через ім'я класу(TestData) і змінної(VALID_PASSWORD) в будь-якому місці програми (тому що створюється спочатку і займає місце поки наша програма виконується)
 }
