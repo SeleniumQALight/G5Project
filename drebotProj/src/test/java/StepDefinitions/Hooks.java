a7b3a82c5ef728dff0e7673e584945fe58a5cb40
KateDrebot <73437573+KateDrebot@users.noreply.github.com>
2022-11-02 17:27:12 -0700

    API + cucumber

--- drebotProj/src/test/java/StepDefinitions/Hooks.java
+++ drebotProj/src/test/java/StepDefinitions/Hooks.java
@@ -1,25 +1,33 @@
 package StepDefinitions;
 
+import api.ApiHelper;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import libs.DriverHelper;
+import libs.TestData;
 import org.apache.log4j.Logger;
 
 public class Hooks {
-
+    ApiHelper apiHelper=new ApiHelper();
     DriverHelper driverHelper = new DriverHelper();
-    Logger logger=Logger.getLogger(getClass());
+    Logger logger = Logger.getLogger(getClass());
 
-    @Before
+    @Before(order = 0)
     public void setUp(Scenario scenario) {
         driverHelper.createWebDriver();
-        logger.info(scenario.getName() +" was started");
+        logger.info(scenario.getName() + " was started");
     }
 
-    @After
+    @After(order = 0)
     public void tearDown(Scenario scenario) {
         driverHelper.closeWebDriver();
-        logger.info(scenario.getName() + " was ended with status "+scenario.getStatus());
+        logger.info(scenario.getName() + " was ended with status " + scenario.getStatus());
+    }
+
+    @Before(value = "@BeforeDeletingAllPostsForDefaultUser", order = 100)
+    @After(value = "@AfterDeletingAllPostsForDefaultUser", order = 50)
+    public void deletingAllPostsForDefaultUser() {
+        apiHelper.deletePostsTillPresent(TestData.VALID_LOGIN,TestData.VALID_PASSWORD);
     }
 }
